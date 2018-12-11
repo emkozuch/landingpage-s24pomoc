7432b45e3a11671bbf70e6ee6e045c4ea51106f9
emkozuch <emkozuch@gmail.com>
2018-12-11 20:47:50 +0100

    php file

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+    anchor.addEventListener('click', function (slide) {
+        slide.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
     });
 });
+
+$(document).ready(function() {
+    $("html").on("submit","#contact_form",function(e){
+      e.preventDefault();
+      $("#send_form_status").html('').hide();
+      var data=$("#contact_form").serialize();
+      $.post("send_form.php",data,function(res){
+        $("#send_form_status").html(res.msg).show();
+        if(res.status==1){ 
+          $("#contact_form")[0].reset();
+        } 
+      });
+    });
+  });
